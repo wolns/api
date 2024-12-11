fb199f4c74bcdeeaeded8d41773c77451e16dd74
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-11 23:36:34 +0400

    added subscriptions to endpoints

--- src/api/v1/users/users.py
+++ src/api/v1/users/users.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, HTTPException
 from fastapi.params import Depends
 
 from src.schemas.user_schemas import UserPostBodySchema, UserResponseSchema
+from src.services.subscription_service import SubscriptionService, get_subscriptions_service
 from src.services.user_service import UserService, get_user_service
 from src.utils.jwt import get_current_user_uuid
 
@@ -40,20 +41,48 @@ async def get_me(
 
 
 @users_router.post("/{user_uuid}/subscribe")
-async def post_subscribe(user_uuid: UUID):
+async def post_subscribe(
+    user_uuid: UUID,
+    current_user_uuid: UUID = Depends(get_current_user_uuid),
+    user_service: UserService = Depends(get_user_service),
+    subscription_service: SubscriptionService = Depends(get_subscriptions_service),
+):
     """
     Subscribe a user
 
+    :param current_user_uuid:
+    :param subscription_service:
+    :param user_service:
     :param user_uuid:
     :return:
     """
+    user = await user_service.get_user_by_uuid(user_uuid)
+    if not user:
+        raise HTTPException(status_code=400, detail="User does not exist")
+
+    if not await subscription_service.subscribe(current_user_uuid, user_uuid):
+        raise HTTPException(status_code=400, detail="Subscription already exists")
 
 
 @users_router.delete("/{user_uuid}/subscribe")
-async def delete_subscribe(user_uuid: UUID):
+async def delete_subscribe(
+    user_uuid: UUID,
+    current_user_uuid: UUID = Depends(get_current_user_uuid),
+    user_service: UserService = Depends(get_user_service),
+    subscription_service: SubscriptionService = Depends(get_subscriptions_service),
+):
     """
     Unsubscribe a user
 
+    :param current_user_uuid:
+    :param user_service:
+    :param subscription_service:
     :param user_uuid:
     :return:
     """
+    user = await user_service.get_user_by_uuid(user_uuid)
+    if not user:
+        raise HTTPException(status_code=400, detail="User does not exist")
+
+    if not await subscription_service.unsubscribe(current_user_uuid, user_uuid):
+        raise HTTPException(status_code=400, detail="Subscription not exists")
