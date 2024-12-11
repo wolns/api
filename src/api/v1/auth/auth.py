5efeba7331764e01fcb3a17926d359da610b3a6e
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-11 22:47:38 +0400

    added jwt take endpoint

--- src/api/v1/auth/auth.py
+++ src/api/v1/auth/auth.py
@@ -1,14 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from fastapi.params import Depends
 
 from src.schemas.token_schemas import TokenPostBodySchema, TokenPostResponseSchema
+from src.services.user_service import UserService, get_user_service
+from src.utils.jwt import create_access_token
 
 auth_router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @auth_router.post("/token")
-async def post_token(body: TokenPostBodySchema) -> TokenPostResponseSchema:
+async def post_token(
+    obj: TokenPostBodySchema, user_service: UserService = Depends(get_user_service)
+) -> TokenPostResponseSchema:
     """
     Get Token by account data
 
     :return:
     """
+    user = await user_service.login_user(obj)
+    if not user:
+        raise HTTPException(status_code=400, detail="User not exists")
+    return TokenPostResponseSchema(access_token=create_access_token({"user_uuid": str(user.uuid)}))
