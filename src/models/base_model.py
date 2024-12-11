525af464fdc2672eb834a89df8a2c7b0219d3612
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-11 17:38:42 +0400

    fixed BaseModel

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from uuid import UUID, uuid4
 
-from sqlalchemy import Column, DateTime, func
 from sqlalchemy.orm import declared_attr
 from sqlmodel import Field, SQLModel
 
@@ -9,10 +8,10 @@ from sqlmodel import Field, SQLModel
 class BaseModel(SQLModel):
     @declared_attr
     def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+        return cls.__name__
 
 
 class BaseUUIDModel(BaseModel):
     uuid: UUID = Field(default_factory=uuid4, primary_key=True, index=True, nullable=False)
-    created_at: datetime = Field(sa_column=Column(DateTime(timezone=True), default=func.now()))
-    updated_at: datetime = Field(sa_column=Column(DateTime(timezone=True), default=func.now(), onupdate=func.now()))
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow, sa_column_kwargs={"onupdate": datetime.utcnow})
