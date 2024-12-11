4604971e73524c1ce3183da8852515a9c50e87dc
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-12 01:27:28 +0400

    reafactored along models and repositories

--- src/repositories/vk_music_account_repository.py
+++ src/repositories/vk_music_account_repository.py
@@ -1,15 +1,6 @@
-from uuid import UUID
-
-from sqlmodel import select
-
 from src.models import VkMusicAccount
-from src.repositories.base_repository import BaseRepository
+from src.repositories.account_repository import BaseAccountRepository
 
 
-class VkMusicAccountRepository(BaseRepository[VkMusicAccount]):
+class VkMusicAccountRepository(BaseAccountRepository):
     model = VkMusicAccount
-
-    async def get_by_user_uuid(self, user_uuid: UUID | str) -> VkMusicAccount:
-        query = select(self.model).where(self.model.user_uuid == user_uuid)
-        response = await self.session.exec(query)
-        return response.one_or_none()
