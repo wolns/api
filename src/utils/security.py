1ccf841fc92e7890af6502b33fc2b13c25e1bafc
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-11 20:58:57 +0400

    async to sync security function

--- src/utils/security.py
+++ src/utils/security.py
@@ -1,7 +1,7 @@
 import hashlib
 
 
-async def get_plain_hash(plain: str | bytes) -> str:
+def get_plain_hash(plain: str | bytes) -> str:
     encoded = plain.encode("utf-8")
     result = hashlib.sha256(encoded)
     return result.hexdigest()
