409d36b6b2e195005aaa7feb6e100292ffd3be74
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-11 20:37:41 +0400

    added subscribe and unsubscribe users

--- src/api/v1/users/users.py
+++ src/api/v1/users/users.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from fastapi import APIRouter
 
 from src.schemas.user_schemas import UserGetResponseSchema, UserPostResponseSchema
@@ -9,6 +11,7 @@ users_router = APIRouter(prefix="/users", tags=["Users"])
 async def create_user() -> UserPostResponseSchema:
     """
     User registration
+
     :return:
     """
 
@@ -19,5 +22,25 @@ async def get_me() -> UserGetResponseSchema:
     Get user information
     TOKEN Required
 
+    :return:
+    """
+
+
+@users_router.post("/{user_uuid}/subscribe")
+async def post_subscribe(user_uuid: UUID):
+    """
+    Subscribe a user
+
+    :param user_uuid:
+    :return:
+    """
+
+
+@users_router.delete("/{user_uuid}/subscribe")
+async def delete_subscribe(user_uuid: UUID):
+    """
+    Unsubscribe a user
+
+    :param user_uuid:
     :return:
     """
