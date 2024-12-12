441af72971076d26e4fe9239fd6faf6ec802c7db
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-12 16:48:22 +0400

    updated track service

--- src/services/track_service.py
+++ src/services/track_service.py
@@ -1,13 +1,22 @@
+from uuid import UUID
+
 from fastapi.params import Depends
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from src.database.database import get_async_session
+from src.models.track_model import Track
 from src.repositories.track_repository import TrackRepository
 
 
 class TrackService:
     def __init__(self, session: AsyncSession):
         self.track_repository = TrackRepository(session)
+
+    async def get_track_by_user_uuid(self, user_uuid: UUID) -> Track | None:
+        return await self.track_repository.get_by_user_uuid(user_uuid)
+
+    async def get_recently_updated_track_by_user_uuid(self, user_uuid: UUID) -> Track | None:
+        return await self.track_repository.get_recently_updated_by_user_uuid(user_uuid)
 
 
 async def get_track_service(session: AsyncSession = Depends(get_async_session)) -> TrackService:
