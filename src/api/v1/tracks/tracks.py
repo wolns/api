72060a21e0e5c5514c4f5b49cbe8af8a4db1341f
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-12 16:48:52 +0400

    added track business service to endpoints

--- src/api/v1/tracks/tracks.py
+++ src/api/v1/tracks/tracks.py
@@ -1,23 +1,40 @@
-from fastapi import APIRouter
+from uuid import UUID
+
+from fastapi import APIRouter, HTTPException
+from fastapi.params import Depends
 
 from src.schemas.track_schemas import TrackGetResponseSchema, TracksGetResponseSchema
+from src.services.track_business_service import TrackBusinessService, get_track_business_service
+from src.utils.jwt import get_current_user_uuid
 
 tracks_router = APIRouter(prefix="/tracks", tags=["Tracks"])
 
 
 @tracks_router.get("/listening-status")
-async def get_listening_status() -> TrackGetResponseSchema:
+async def get_listening_status(
+    current_user_uuid: UUID = Depends(get_current_user_uuid),
+    track_business_service: TrackBusinessService = Depends(get_track_business_service),
+) -> TrackGetResponseSchema:
     """
     Getting random listening status.
 
     :return:
     """
+    track = await track_business_service.get_subscribed_track_by_user_uuid(current_user_uuid)
+    if track:
+        return await track_business_service.to_response_model(track)
+    raise HTTPException(detail="Track not found", status_code=404)
 
 
 @tracks_router.get("/listening-statuses")
-async def get_listening_statuses() -> TracksGetResponseSchema:
+async def get_listening_statuses(
+    current_user_uuid: UUID = Depends(get_current_user_uuid),
+    track_business_service: TrackBusinessService = Depends(get_track_business_service),
+) -> TracksGetResponseSchema:
     """
     Getting all listening statuses.
 
     :return:
     """
+    tracks = await track_business_service.get_subscribed_tracks_by_user_uuid(current_user_uuid)
+    return TracksGetResponseSchema(tracks=[await track_business_service.to_response_model(track) for track in tracks])
