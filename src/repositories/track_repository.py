0bcecf850114799ba6227be735659920957c40d9
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-12 16:48:02 +0400

    added timezone into packages

--- src/repositories/track_repository.py
+++ src/repositories/track_repository.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timedelta
 from uuid import UUID
 
 from sqlmodel import select
 from typing_extensions import TypeVar
 
+from src.core.timezone import tz
 from src.models.track_model import Track
 from src.repositories.base_repository import BaseRepository
 
@@ -13,6 +15,12 @@ class TrackRepository(BaseRepository[Track]):
     model = Track
 
     async def get_by_user_uuid(self, user_uuid: UUID | str) -> T | None:
-        query = select(self.model).where(self.model.user_uuid == user_uuid)
+        query = select(Track).where(Track.user_uuid == user_uuid)
+        response = await self.session.exec(query)
+        return response.one_or_none()
+
+    async def get_recently_updated_by_user_uuid(self, user_uuid: UUID, minutes: int = 1) -> T | None:
+        threshold_time = datetime.now(tz) - timedelta(minutes=minutes)
+        query = select(Track).where(Track.user_uuid == user_uuid, Track.updated_at > threshold_time)
         response = await self.session.exec(query)
         return response.one_or_none()
