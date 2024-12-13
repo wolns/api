6a668725cb8dd4caea1b251a95556c630384f57f
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-13 18:17:53 +0400

    updated track update service

--- src/services/track_update_service.py
+++ src/services/track_update_service.py
@@ -11,22 +11,28 @@ from src.services.music_service import MusicService
 from src.services.music_services.spotify_service import SpotifyService
 from src.services.music_services.yandex_music_service import YandexMusicService
 from src.services.track_service import TrackService
+from src.services.user_service import UserService
 
 
 class TrackUpdateService:
     def __init__(
-        self, track_service: TrackService, account_service: AccountService, music_services: list[MusicService]
+        self,
+        track_service: TrackService,
+        account_service: AccountService,
+        user_service: UserService,
+        music_services: list[MusicService],
     ) -> None:
         self.track_service = track_service
         self.account_service = account_service
+        self.user_service = user_service
         self.music_services = {service.service_type: service for service in music_services}
 
     async def update_tracks(self):
-        update_tracks = await self.track_service.get_update_tracks()
-        for update_track in update_tracks:
-            await self.update_user_track(update_track.user_uuid, update_track)
+        update_track_users = await self.user_service.get_users_update_track()
+        for user in update_track_users:
+            await self.update_user_track(user.uuid)
 
-    async def update_user_track(self, user_uuid: UUID, track: Track | None = None):
+    async def update_user_track(self, user_uuid: UUID):
         accounts = [
             await self.account_service.get_yandex_music_account(user_uuid),
             await self.account_service.get_spotify_account(user_uuid),
@@ -35,14 +41,14 @@ class TrackUpdateService:
         for account in accounts:
             if account and account.service_type in self.music_services:
                 music_service = self.music_services[account.service_type]
-                if await self.update_user_track_with_service(account, music_service, track):
-                    break
+                try:
+                    if await self.update_user_track_with_service(account, music_service):
+                        break
+                except:
+                    pass
 
-    async def update_user_track_with_service(
-        self, account: BaseAccount, music_service: MusicService, track: Track | None = None
-    ) -> Track | None:
-        if not track:
-            track = await self.track_service.get_track_by_user_uuid(account.user_uuid)
+    async def update_user_track_with_service(self, account: BaseAccount, music_service: MusicService) -> Track | None:
+        track = await self.track_service.get_track_by_user_uuid(account.user_uuid)
         current_track = await music_service.get_current_track(account)
         if current_track:
             if not track:
@@ -58,8 +64,9 @@ class TrackUpdateService:
 async def get_track_update_service(session: AsyncSession = Depends(get_async_session)) -> TrackUpdateService:
     track_service = TrackService(session)
     account_service = AccountService(session)
+    user_service = UserService(session)
     music_services = [
         YandexMusicService(),
         SpotifyService(),
     ]
-    return TrackUpdateService(track_service, account_service, music_services)
+    return TrackUpdateService(track_service, account_service, user_service, music_services)
