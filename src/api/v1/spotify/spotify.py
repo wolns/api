f74056704aefa1bc141e731b3d67f91a20303b5a
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-13 14:01:12 +0400

    fixed spotify endpoints

--- src/api/v1/spotify/spotify.py
+++ src/api/v1/spotify/spotify.py
@@ -1,22 +1,23 @@
 from fastapi import APIRouter, Depends, Query
 
+from src.schemas.account_schemas import SpotifyAccountBodySchema
 from src.schemas.spotify_schemas import (
     SpotifyAuthResponseSchema,
     SpotifyCallbackResponseSchema,
-    SpotifyCurrentTrackResponseSchema,
 )
-from src.services.spotify_service import SpotifyService, get_spotify_service
+from src.schemas.track_schemas import TrackBaseInfo
+from src.services.music_services.spotify_service import SpotifyService, get_spotify_service
 
 spotify_router = APIRouter(prefix="/spotify", tags=["Spotify"])
 
 
-@spotify_router.get("/login", response_model=SpotifyAuthResponseSchema)
+@spotify_router.get("/login")
 async def spotify_login(spotify_service: SpotifyService = Depends(get_spotify_service)) -> SpotifyAuthResponseSchema:
     auth_url = spotify_service.get_auth_url()
     return SpotifyAuthResponseSchema(auth_url=auth_url)
 
 
-@spotify_router.get("/callback", response_model=SpotifyCallbackResponseSchema)
+@spotify_router.get("/callback")
 async def spotify_callback(
     code: str = Query(...), spotify_service: SpotifyService = Depends(get_spotify_service)
 ) -> SpotifyCallbackResponseSchema:
@@ -27,8 +28,10 @@ async def spotify_callback(
     )
 
 
-@spotify_router.get("/current-track", response_model=SpotifyCurrentTrackResponseSchema)
+# TEST PURPOSES ONLY
+@spotify_router.get("/current-track")
 async def get_current_track(
     access_token: str = Query(...), spotify_service: SpotifyService = Depends(get_spotify_service)
-) -> SpotifyCurrentTrackResponseSchema:
-    return await spotify_service.get_current_track(access_token)
+) -> TrackBaseInfo:
+    test = SpotifyAccountBodySchema(access_token=access_token, refresh_token=access_token)
+    return await spotify_service.get_current_track(test)
