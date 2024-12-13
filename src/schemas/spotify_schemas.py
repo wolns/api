cf06611b086942ed628a8b22d777927ec7020546
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-13 14:00:10 +0400

    removed unused complex schemas

--- src/schemas/spotify_schemas.py
+++ src/schemas/spotify_schemas.py
@@ -1,7 +1,5 @@
 from pydantic import BaseModel
 
-# Egor, sorry for this shit ahahahaha
-
 
 class SpotifyAuthResponseSchema(BaseModel):
     auth_url: str
@@ -10,10 +8,3 @@ class SpotifyAuthResponseSchema(BaseModel):
 class SpotifyCallbackResponseSchema(BaseModel):
     access_token: str
     refresh_token: str
-
-
-class SpotifyCurrentTrackResponseSchema(BaseModel):
-    title: str
-    artists: str
-    cover_url: str
-    is_playing: bool
