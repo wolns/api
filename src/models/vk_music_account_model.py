430a502c3d59c25d91ef0dbe1342ede4f770b93a
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-13 17:20:56 +0400

    added service_type to account models

--- src/models/vk_music_account_model.py
+++ src/models/vk_music_account_model.py
@@ -1,8 +1,9 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, ClassVar
 
 from sqlalchemy import Column, String
 from sqlmodel import Field, Relationship
 
+from src.schemas.track_schemas import ServiceType
 from .account_model import BaseAccount
 
 if TYPE_CHECKING:
@@ -10,6 +11,8 @@ if TYPE_CHECKING:
 
 
 class VkMusicAccount(BaseAccount, table=True):
+    service_type: ClassVar[ServiceType] = ServiceType.VK_MUSIC
+
     token: str = Field(sa_column=Column(String(400), nullable=False))
 
     user: "User" = Relationship(
