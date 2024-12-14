e167ae1215ad05eb3af421d62c6a177a9462de70
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-14 15:13:47 +0400

    extended account models

--- src/models/account_model.py
+++ src/models/account_model.py
@@ -4,10 +4,12 @@ from uuid import UUID
 from sqlmodel import Field
 
 from src.models.base_model import BaseUUIDModel
+from src.schemas.account_schemas import AccountBodySchema
 from src.schemas.track_schemas import ServiceType
 
 
 class BaseAccount(BaseUUIDModel):
     service_type: ClassVar[ServiceType]
+    schema: ClassVar[AccountBodySchema]
 
     user_uuid: UUID = Field(default=None, foreign_key="User.uuid")
