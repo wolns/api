9610e3eb51c1817cb9a8e9e30631f4b5c8bef3c1
Egor Fadeev <116910468+fadegor05@users.noreply.github.com>
2024-12-17 16:53:36 +0400

    fixed models import

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 from .spotify_account_model import SpotifyAccount
 from .subscription_model import Subscription
+from .track_model import Track
 from .user_model import User
 from .vk_music_account_model import VkMusicAccount
 from .yandex_music_account_model import YandexMusicAccount
